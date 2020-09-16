5cbb59ab7eba26817f73aa8c17e9d89579850909
Marta H <65173504+martahdez90@users.noreply.github.com>
2020-09-16 13:53:14 +0200

    2

--- metodos_array.js
+++ metodos_array.js
@@ -1,11 +1,12 @@
 let peliculas = ["Harry Potter", "Matilda", "Spiderman"];
 let personajes = ["Hermione", "madrastra", "Perer Parker"];
 let cine = [peliculas, personajes]
-const plength = personajes.length + 3;
+const plength = personajes.length;
+console.log(plength);
 
 function mostrar() {
     document.write(`las peliculas de este cine son ${peliculas.toString()} <br>
-    y los personajes son ${personajes.toString()}`)  
+    y los personajes son ${personajes.toString()}<br><br>`)  
 }
 
 mostrar();
@@ -13,13 +14,13 @@ mostrar();
 
 
 function addpersonajes(personajes) {
-    for (i = 0; i < plength; i++){
+    for (i = 0; i < 8; i++){
     let length2 = personajes.length;
     console.log(`comienzo de longitud en ${length2}`)
         let newPersonaje = prompt("escribe un personaje"); 
         personajes.push(newPersonaje)
     console.log(i);
-    console.log(length);
+    console.log(length2);
 }
 }
 
@@ -30,7 +31,7 @@ mostrar()
 
 function addpeliculas(peliculas) {
    while (peliculas.length < personajes.length) {
- let newPeliculas = prompt("introduce una pelicua")
+ let newPeliculas = prompt("introduce una pelicula")
     peliculas.push(newPeliculas); 
     return peliculas.length + "" + personajes.length;
 } 
