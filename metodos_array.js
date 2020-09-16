ee024318562cea7c819ea7e1b9de59bc5afff0dc
Marta Hernandez <martahdez90@users.noreply.github.com>
2020-09-16 20:35:33 +0200

    array

--- metodos_array.js
+++ metodos_array.js
@@ -1,43 +1,100 @@
-let peliculas = ["Harry Potter", "Matilda", "Spiderman"];
+'use strict'
+
+let peliculas = ["Harry Potter", "Matilda", "Spiderman", "Batman"];
 let personajes = ["Hermione", "madrastra", "Perer Parker"];
 let cine = [peliculas, personajes]
-const plength = personajes.length;
-console.log(plength);
 
-function mostrar() {
-    document.write(`las peliculas de este cine son ${peliculas.toString()} <br>
-    y los personajes son ${personajes.toString()}<br><br>`)  
+
+
+//PUSH and propiedad LENGTH
+// let elemento = "";
+
+// do {
+//     elemento = prompt("Introduce una película: "); 
+//     peliculas.push(elemento);
+//     console.log(peliculas.length)
+// }   while (elemento != "ACABAR") 
+
+
+//PUSH, POP, TOSTRING
+// peliculas.push("Bambi")
+// document.write(`<div class="texto">la ultima pelicula era: ${peliculas.pop()} <br><br> ${peliculas.toString()}</div> `)
+
+// INDEXOF, PUSH, POP, SPLICE, SHIFT, LASTINDEXOF
+
+
+peliculas.push("Bambi"); //añadeal final ["Harry Potter", "Matilda", "Spiderman, "Bambi"];
+document.write(peliculas + "<br>")
+peliculas.shift(); //elimina el 1º ["Matilda", "Spiderman, "Bambi"]
+document.write(peliculas + "<br>");
+let index = peliculas.indexOf("Batman");
+if (index != 1) {
+    peliculas.splice(index, 1); //elimina a partir del del indice ["Matilda", "Bambi"]
 }
 
-mostrar();
+document.getElementById("texto").innerHTML = peliculas.toString();
+document.write(peliculas + "<br>");
+peliculas.pop(); //elimina el último ["Matilda"]
+document.write(peliculas + "<br>");
+peliculas.unshift("Matilda") //añade al principio ["Matilda, "Spiderman", "Matilda]
+document.write(peliculas + "<br>");
+index = peliculas.lastIndexOf("Matilda");
+peliculas.splice(index, 1); //elimina el indice
+document.write(peliculas + "<br>"); //["Matilda", "Spiderman"]
+peliculas.push("Pitufos"); //["Matilda", "Spiderman", "Pitufos"]
+document.write(peliculas.join(" o ") + "<br>"); //convierte a string añadiendo el separador indicado
 
+//ordenar arrray 
+peliculas.sort();//alfabeticamente
+document.write(peliculas + "<br>");
+peliculas.reverse();
+document.write(peliculas + "<br>");
 
+//recorrer array
 
-function addpersonajes(personajes) {
-    for (i = 0; i < 8; i++){
-    let length2 = personajes.length;
-    console.log(`comienzo de longitud en ${length2}`)
-        let newPersonaje = prompt("escribe un personaje"); 
-        personajes.push(newPersonaje)
-    console.log(i);
-    console.log(length2);
-}
-}
-
-addpersonajes(personajes)
-
-
-mostrar()
-
-function addpeliculas(peliculas) {
-   while (peliculas.length < personajes.length) {
- let newPeliculas = prompt("introduce una pelicula")
-    peliculas.push(newPeliculas); 
-    return peliculas.length + "" + personajes.length;
-} 
+for (let pelicula in peliculas) {
+    document.write(`<li> ${pelicula} </li>`)//muestra el indice
 }
 
-addpeliculas(peliculas);
+// for (let pelicula in peliculas) {
+//     document.write(`<li> ${peliculas[pelicula]} </li>`)//muestra el indice
+// }
+
+for (let pelicula of peliculas) {
+    document.write(`<li> ${pelicula} </li>`)
+}
+
+document.write(`<h2 class ="h2 text-success text-center">Comienzan las busquedas</h2><br><br>`)
+
+//busquedas
+
+let busqueda = peliculas.find((pelicula) => {
+    return pelicula == "Matilda";
+})
+
+busqueda = peliculas.find(pelicula => pelicula == "Matilda");
+
+document.write(busqueda + "<br>")
+
+let eliminar = peliculas.findIndex(pelicula => pelicula == "Matilda")
+document.write(eliminar + "<br>")
+peliculas.splice(eliminar, 1);
+document.write(peliculas + "<br>")
+
+peliculas.push("Bambi2");
+peliculas.push("Pocahontas");
+peliculas.unshift("Pocahontas");
+document.write(peliculas + "<br>")
 
 
-mostrar();
+//TO STRING
+
+// function mostrar() {
+//     document.write(`las peliculas de este cine son ${peliculas.toString()} <br>
+//     y los personajes son ${personajes.toString()}<br><br>`)  
+// }
+
+// mostrar();
+
+
+
---
+++ metodos_string.js
@@ -0,0 +1,7 @@
+let frutas = "manzana, naranja, melocotón, plátano"
+
+document.write(frutas.split(", "));//convertir en un array todo lo que esté detrás de , 
+
+let frase = "los niños juegan al futbol"
+
+document.write(frase.split(" "));
