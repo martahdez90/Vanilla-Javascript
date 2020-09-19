71bb2eb405898d13aedd6f18d6b22beefc166f8f
Marta Hernandez <martahdez90@users.noreply.github.com>
2020-09-19 18:48:03 +0200

    funcionalidad

---
+++ practicas/games/memory/app.js
@@ -0,0 +1,130 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //card options
+    const wizard = {
+        name: 'wizard',
+        img: './images/1wizard.svg'
+    }
+    const dwarf = {
+        name: 'dwarf',
+        img: './images/2dwarf.svg'
+    }
+    const elf = {
+        name: 'elf',
+        img: './images/3elf.svg'
+    }
+    const queen = {
+        name: 'queen',
+        img: './images/4queen.svg'
+    }
+    const prince = {
+        name: 'prince',
+        img: './images/5prince.svg'
+    }
+    const frogPrince = {
+        name: 'frogPrince',
+        img: './images/6fprince.svg'
+    }
+    const fairy = {
+        name: 'fairy',
+        img: './images/7fairy.svg'
+    }
+    const unicorn = {
+        name: 'unicorn',
+        img: './images/8unicorn.svg'
+    }
+
+
+    const cardArray = [wizard, dwarf, elf, queen, prince, frogPrince, fairy, unicorn, wizard, dwarf, elf, queen, prince, frogPrince, fairy, unicorn]
+
+    //cardArray.sort(() => 0.5 - Math.random())
+
+    const grid = document.querySelector('.grid')
+    const resultDisplay = document.querySelector('#result')
+    let cardsChosen = []
+    let cardsChosenId = []
+    const cardsWon = []
+
+    //alertas 
+
+
+    // Crear tablero
+    function createBoard() {
+        for (let i = 0; i < cardArray.length; i++) {
+            let card = document.createElement('img')
+            card.setAttribute('src', './images/colors.svg')
+            card.setAttribute('data-id', i)
+            card.classList.add('mystyle')
+            card.addEventListener('click', flipCard)
+            grid.appendChild(card)
+        }
+    }
+
+    // //check for matches
+    function checkForMatch() {
+
+        let cards = document.querySelectorAll('img')
+        const optionOneId = cardsChosenId[0]
+        const optionTwoId = cardsChosenId[1]
+
+        if (optionOneId == optionTwoId) {
+            let img1 = cards[optionOneId].setAttribute('src', './images/colors.svg')
+            let img2 = cards[optionTwoId].setAttribute('src', './images/colors.svg')
+            img1.classList.add('mystyle')
+            img2.classList.add('mystyle')
+            alert('You have clicked the same image!')
+        }
+        else if (cardsChosen[0] === cardsChosen[1]) {
+            cards[optionOneId].setAttribute('src', './images/white.svg')
+            cards[optionTwoId].setAttribute('src', './images/white.svg')
+            cards[optionOneId].removeEventListener('click', flipCard)
+            cards[optionTwoId].removeEventListener('click', flipCard)
+            cards[optionOneId].classList.add('mystyle')
+            cards[optionTwoId].classList.add('mystyle')
+            cardsWon.push(cardsChosen)
+        } else {
+            cards[optionOneId].setAttribute('src', './images/colors.svg')
+            cards[optionTwoId].setAttribute('src', './images/colors.svg')
+            cards[optionOneId].classList.add('mystyle')
+            cards[optionTwoId].classList.add('mystyle')
+            // Swal.fire({
+            //     title: '¡oh no!',
+            //     text: 'Inténtalo otra vez',
+            //     icon: 'error',
+            //     confirmButtonText: '¡Voy!'
+            //   })
+        }
+        cardsChosen = []
+        cardsChosenId = []
+        resultDisplay.textContent = cardsWon.length
+        if (cardsWon.length === cardArray.length / 2) {
+            Swal.fire({
+                title: '¡Enhorabuena!',
+                text: 'Lo has conseguido',
+                icon: 'success',
+                confirmButtonText: '¡Voy!'
+            })
+        }
+    }
+
+    //flip your card
+    function flipCard() {
+        let cardId = this.getAttribute('data-id')
+
+        cardsChosen.push(cardArray[cardId].name)
+        cardsChosenId.push(cardId)
+        this.setAttribute('src', cardArray[cardId].img)
+
+        if (cardsChosen.length === 2) {
+            setTimeout(checkForMatch, 500)
+        }
+    }
+
+    createBoard()
+
+
+})
+
+
+
+
+
